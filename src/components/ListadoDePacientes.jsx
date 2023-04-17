b2f00f30351e947c6f5700e3142d26b30789c65a
Pab-Fernandez <pablower@gmail.com>
2023-04-17 05:16:45 -0300

    minor style changes,mostly testing

--- src/components/ListadoDePacientes.jsx
+++ src/components/ListadoDePacientes.jsx
@@ -2,7 +2,7 @@ const ListadoDePacientes = () => {
     return (
       <div className="w 1/2 lg:w-3/5">
         
-        <h2 className="font-bold">Listado de Pacientes</h2>
+        <h2 className="font-black text-3xl">Listado de Pacientes</h2>
         
       </div>
     );
