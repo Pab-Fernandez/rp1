270f449a6d26862e1c15bca1ef0676db961f9804
Pab-Fernandez <pablower@gmail.com>
2023-04-21 11:19:44 -0300

    added hooks

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,8 +1,19 @@
 import {useState, useEffect} from 'react';
 
-
-
 const Formulario = () => {
+  const [nombre, setNombre] =  useState ('');
+  const [propietario, setPropietario] =  useState ('');
+  const [email, setEmail] =  useState ('');
+  const [fecha, setFecha] =  useState ('');
+  const [sintomas, setSintomas] =  useState ('');
+  const handleSubmit = (e) => {
+    e.preventDefault ();
+    console.log ('Enviando formulario')
+
+  }
+
+
+
   return (
     <div className="md:w-1/2 lg:w-2/5">
       <h2 className=" font-black text-3xl text-center">Seguimiento de Pacientes</h2>
@@ -10,7 +21,9 @@ const Formulario = () => {
       Añadir pacientes y {''}
       <span className="font-bold text-red-950">Administrar</span>
           </p>
-  <form className="bg-white shadow-md rounded py-10 px-5">
+  <form
+  onSubmit={handleSubmit}
+   className="bg-white shadow-md rounded py-10 px-5">
     <div className="mb-5">
       <label htmlFor="paciente" className="block text-gray-80 uppercase font-bold">Nombre paciente</label>
       <input
@@ -18,7 +31,8 @@ const Formulario = () => {
       type="text"
       placeholder="Nombre del paciente"
       className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded"
-    
+      value= {nombre}
+      onChange= { (e) => setNombre(e.target.value)}    
       />
     </div>
     <div className="mb-5">
@@ -28,7 +42,8 @@ const Formulario = () => {
       type="text"
       placeholder="Nombre del humano"
       className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded"
-    
+      value= {propietario}
+      onChange= { (e) => setPropietario(e.target.value)}       
       />
     </div>
     <div className="mb-5">
@@ -38,7 +53,8 @@ const Formulario = () => {
       type="email"
       placeholder="mail de contacto"
       className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded"
-    
+      value= {email}
+      onChange= { (e) => setEmail(e.target.value)}   
       />
     </div>
     <div className="mb-5">
@@ -47,7 +63,8 @@ const Formulario = () => {
       id= "alta" 
       type="date"
       className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded"
-    
+      value= {fecha}
+      onChange= { (e) => setFecha(e.target.value)}   
       />
     </div>
     <div className="mb-5">
@@ -56,7 +73,11 @@ const Formulario = () => {
     <textarea
      id="sintomas"    
      placeholder="descripción del caso"
-     className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded" />
+     className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded" 
+     value= {sintomas}
+     onChange= { (e) => setSintomas(e.target.value)}   
+     />
+      
     </div>
     <input 
     type="submit" 
@@ -70,4 +91,3 @@ const Formulario = () => {
 }
 
 export default Formulario;
-
